20c30bec8ee3069a6e240e298a10304f67b05171
zzangkkmin <zzangkkmin@naver.com>
2024-11-19 10:39:52 +0900

    UPDATE - Test 변경 및 추가

--- scvs/src/test/java/com/team3/scvs/DBConnTests.java
+++ scvs/src/test/java/com/team3/scvs/DBConnTests.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
 import java.util.List;
 import java.util.Map;
 
@@ -16,11 +17,28 @@ public class DBConnTests {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     void testSelectTable(){
         String sql = "SELECT * FROM test";
         List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
 
         assertThat(maps).isNotNull();
+        assertThat(maps.size()).isNotZero();
+    }
+
+    @Test
+    void testConnection() {
+        try {
+            String result = jdbcTemplate.queryForObject("SELECT VERSION()", String.class);
+            System.out.println("DB 연결 성공!\nMySQL 버전: " + result);
+            String url = dataSource.getConnection().getMetaData().getURL();
+            System.out.println("DB 연결 URL: " + url);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("DB 연결 실패!");
+        }
     }
 }
