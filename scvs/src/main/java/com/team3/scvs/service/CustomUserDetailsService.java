df2cdcdbc7c805f04d785cdf815803a548d6dcfb
KimGunWoo <KGW561@gmail.com>
2024-11-28 15:14:18 +0900

    [ADMIN-ACCOUNT-ROLE] 관리자추가에 따른 role기능 추가
    로그인, 회원가입등 기존 기능의 role(역할)추가
    사용자 - ROLE_USER
    관리자 - ROLE_ADMIN

--- scvs/src/main/java/com/team3/scvs/service/CustomUserDetailsService.java
+++ scvs/src/main/java/com/team3/scvs/service/CustomUserDetailsService.java
@@ -5,11 +5,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 
 @Slf4j
@@ -21,13 +26,23 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // 사용자 정보 가져오기
+        // 이메일로 사용자 정보 가져오기
         return userRepository.findByEmail(username)
-                .map(user -> User.builder()
-                        .username(user.getEmail())
-                        .password(user.getPassword())
-                        .build())
+                .map(userEntity -> {
+                    // 사용자 권한을 GrantedAuthority 타입의 리스트로 변환
+                    List<GrantedAuthority> authorities = Collections.singletonList(
+                            new SimpleGrantedAuthority(userEntity.getUserrole()) // 권한 설정
+                    );
+
+                    // userEntity를 UserDetails 변환하여 객체 반환
+                    return User.builder()
+                            .username(userEntity.getEmail()) // 이메일(DB)을 username(Security)으로 사용
+                            .password(userEntity.getPassword()) // 비밀번호
+                            .authorities(authorities) // 권한
+                            .build();
+                })
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username));
     }
+
 }
 
