0405b8a41e937f23407d12b6d6754e7dc0b46dfc
zzangkkmin <zzangkkmin@naver.com>
2024-12-02 13:40:19 +0900

    [MAIN-HOME] 경제 지표 표시

--- scvs/src/main/java/com/team3/scvs/controller/BaseController.java
+++ scvs/src/main/java/com/team3/scvs/controller/BaseController.java
@@ -1,6 +1,8 @@
 package com.team3.scvs.controller;
 
+import com.team3.scvs.dto.ForexDTO;
 import com.team3.scvs.dto.IndexDTO;
+import com.team3.scvs.service.ForexService;
 import com.team3.scvs.service.IndexService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,6 +18,9 @@ public class BaseController {
     @Autowired
     private IndexService indexService;
 
+    @Autowired
+    private ForexService forexService;
+
     @ModelAttribute("isLoggedIn") // 자동으로 설정
     public boolean setDefaultIsLoggedIn() {
         return false; // 기본값 설정
@@ -24,8 +29,10 @@ public class BaseController {
     @GetMapping("/")
     public String mainPage(Model model) {
         List<IndexDTO> indices = indexService.getIndices();
+        ForexDTO krwUsdForex = forexService.getKrwUsdForex();
 
         model.addAttribute("indices", indices);
+        model.addAttribute("forex", krwUsdForex);
         return "index";
     }
 
