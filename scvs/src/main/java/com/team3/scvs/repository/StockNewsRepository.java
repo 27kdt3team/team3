4c821a186f30fa8694dc7403a0aac06684ac021c
JOOYEON PARK <juyeon0716@naver.com>
2024-12-07 14:51:45 +0900

    [USER-STOCK-NEWS] 주식 뉴스 상세 페이지 구현
    1. 주식 뉴스 상세 페이지 구현

---
+++ scvs/src/main/java/com/team3/scvs/repository/StockNewsRepository.java
@@ -0,0 +1,14 @@
+package com.team3.scvs.repository;
+
+import com.team3.scvs.entity.StockNewsEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockNewsRepository extends JpaRepository<StockNewsEntity, Long> {
+
+    Page<StockNewsEntity> findByTickerId(Long tickerId, Pageable pageable);
+
+}
