127742b26cfead34527364d5834332c853526492
zzangkkmin <zzangkkmin@naver.com>
2024-12-16 15:55:10 +0900

    [HOTFIX] 미사용 소스 제거중

--- scvs/src/main/java/com/team3/scvs/entity/PSAEntity.java
+++ scvs/src/main/java/com/team3/scvs/entity/PSAEntity.java
@@ -1,6 +1,7 @@
 package com.team3.scvs.entity;
 
 import jakarta.persistence.*;
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
